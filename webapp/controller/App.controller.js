8a986ec5c96f0b0d2854f14250cbd04d01a3c466
gregorwolf <gregor.wolf@gmail.com>
2016-08-23 10:23:32 +0200

    Testing with local Web IDE using a Button instead of BarcodeScannerButton

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -12,22 +12,52 @@ sap.ui.define([
 
 		scanSuccessCallback: function(result) {
 			var discountCode = result.mParameters.text;
+			this._checkDiscountCode(discountCode);
+		},
+		
+		onEnterDiscountCode: function(result) {
+			var oLayout = sap.ui.xmlfragment("de.linuxdozent.gittest.adminui.view.EnterDiscountCode", this);
+			
+			// get the view and add the layout as a dependent. Since the layout is being put
+			// into an aggregation any possible binding will be 'forwarded' to the layout.
+			var oView = this.getView();
+			oView.addDependent(oLayout);
+			
+			sap.m.MessageBox.show(oLayout, {
+				icon : sap.m.MessageBox.Icon.WARNING,
+				title : "Title of first MessageBox",
+				actions : [sap.m.MessageBox.Action.OK, sap.m.MessageBox.Action.CANCEL],
+				onClose : this._onCloseEnterDiscountCode.bind(this),
+				dialogId : "messageBoxId"
+			});
+		},
+		
+	    scanFailCallback: function(error) {
+	        this._MessageToast("Scanning failed: " + JSON.stringify(error));
+	    },
+	    
+	    _onCloseEnterDiscountCode: function(oAction) {
+			if ( oAction === sap.m.MessageBox.Action.OK ) {
+				/** @type sap.m.Input */
+				var oDiscountCode = sap.ui.getCore().byId("discountCodeId");
+				var discountCode = oDiscountCode.getValue();
+				this._checkDiscountCode(discountCode);
+			}
+		},
+		
+		_checkDiscountCode: function(discountCode) {
 			var checkUrl = "/HANAMDC/de/linuxdozent/gittest/odata/checkDiscountCode.xsjs";
+			/** @type sap.ui.model.odata.ODataModel */
 			this.model = this.getView().getModel();
 			var token = this.model.getSecurityToken();
 			/** @type sap.ui.model.json.JSONModel */
 			var oModel = new sap.ui.model.json.JSONModel();
 			var mHeaders = Array();
 			mHeaders["X-CSRF-Token"] = token;
-			var oData = {};
-			oData.SHA256HASH = discountCode;
+			var sData = "SHA256HASH=" + encodeURIComponent(discountCode);
 			oModel.attachRequestCompleted(this._discountCodeRequestCompleted.bind(this));
-			oModel.loadData(checkUrl, JSON.stringify(oData), true, 'POST', false, false, mHeaders);
+			oModel.loadData(checkUrl, sData, true, 'GET', false, false, mHeaders);
 		},
-		
-	    scanFailCallback: function(error) {
-	        this._MessageToast("Scanning failed: " + JSON.stringify(error));
-	    },
 	    
 	    _discountCodeRequestCompleted: function(oEvent) {
 				/** @type sap.ui.model.json.JSONModel */
@@ -35,7 +65,8 @@ sap.ui.define([
 				this.discount = model.getProperty("/OUTC")[0];
 				var sMessage = "The scanned code has a value of " + this.discount.DiscountValue
 							+ " belongs to customer " + this.discount.UserName
-							+ " redemption status " + this.discount.Redeemed;
+							+ " redemption status " + (this.discount.Redeemed === "Y" ? "not valid" : "is valid")
+							+ " . Do you want to redeem the code now?";
 				MessageBox.show(
 					sMessage, {
 						icon: sap.m.MessageBox.Icon.INFORMATION,
@@ -48,10 +79,25 @@ sap.ui.define([
 		
 		_messageBoxConfirmed: function(oAction) {
 			if(oAction === sap.m.MessageBox.Action.YES){
-				this._MessageToast(JSON.stringify("Discount code was redeemed."));
+				this._redeemDiscountCode().bind(this);
 			} else {
 				this._MessageToast(JSON.stringify("Discount code still valid."));
 			}
+		},
+		
+		_redeemDiscountCode: function() {
+			var that = this;
+			var sPath = "/DiscountCode('" + this.discount.ID + "')";
+			this.model.setProperty(sPath + "/SHA256HASH", this.discount.SHA256HASH);
+			this.model.attachEventOnce("batchRequestCompleted", function(oEvent2) {
+				var oParams = oEvent2.getParameters();
+				if (oParams.success) {
+					that._MessageToast("Discount code was redeemed.");
+				} else {
+					that._MessageToast("There was a problem redeeming the discount code");
+				}
+			});
+			this.model.submitChanges();
 		},
 	    
 	    _MessageToast: function(sMessage) {
